065acde3f1a6f839415f57c8bd9eef56f664f9b7
Nifilia <anderokore@gmail.com>
2023-06-04 21:36:24 +0300

    Homepage Made

--- project/src/screens/app/Home/styles.js
+++ project/src/screens/app/Home/styles.js
@@ -3,6 +3,9 @@ import { colors } from "../../../utils/colors";
 
 export const styles = StyleSheet.create({
     container: {
-        padding: 24,
+        padding: 16
     },
+    list: {
+        paddingVertical: 24
+    }
 })
